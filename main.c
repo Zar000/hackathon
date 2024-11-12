0250835826c0dad357f31f21ecc71255c6580914
hager3737 <johannbonde@hotmail.se>
2024-11-12 12:53:38 +0100

    test

--- main.c
+++ main.c
@@ -10,19 +10,16 @@
 #include <time.h>
 #include <errno.h>    
 
-
-void hideCursor(){
+void hideCursor() {
     printf("\e[?25l");
 }
 
 /* msleep(): Sleep for the requested number of milliseconds. */
-int msleep(long msec)
-{
+int msleep(long msec) {
     struct timespec ts;
     int res;
 
-    if (msec < 0)
-    {
+    if (msec < 0) {
         errno = EINVAL;
         return -1;
     }
@@ -40,21 +37,34 @@ int msleep(long msec)
 #define ROWS 10 // 2-9
 #define COLS 30 // 2-29
 #define WALL '#'
+#define MAX_SNAKE_LENGTH 100
 
-typedef struct{
+typedef enum {
+    KeyboardDir_Left,    
+    KeyboardDir_Right,
+    KeyboardDir_Up,
+    KeyboardDir_Down,
+} KeyboardDir;
+
+typedef struct {
     int X;
     int Y;
-}Snake;
+} Position;
 
-typedef struct{
+typedef struct {
+    Position body[MAX_SNAKE_LENGTH];
+    int length;
+} Snake;
+
+typedef struct {
     int X;
     int Y;
-}Position;
+} Apple;
 
 Position getNextPosition(Snake snake, KeyboardDir direction) {
     Position nextPosition;
-    nextPosition.X = snake.X;
-    nextPosition.Y = snake.Y;
+    nextPosition.X = snake.body[0].X;
+    nextPosition.Y = snake.body[0].Y;
 
     if (direction == KeyboardDir_Up) {
         nextPosition.Y = (nextPosition.Y == 2) ? ROWS + 1 : nextPosition.Y - 1;
@@ -71,119 +81,122 @@ Position getNextPosition(Snake snake, KeyboardDir direction) {
 
 
 #define clrscr() printf("\e[1;1H\e[2J")
-void gotoxy(int x,int y){
-
-    printf("%c[%d;%df",0x1B,y,x);
+void gotoxy(int x, int y) {
+    printf("%c[%d;%df", 0x1B, y, x);
 }
 
 int score = 0;
 char *scoreText = "Score : ";
 
-
-void updateScore(){
+void updateScore() {
     gotoxy(0, 30);
     printf("\r");
-    printf("%s",scoreText);
+    printf("%s", scoreText);
     printf("%d", score);
 }
 
-void increaseScore(){
+void increaseScore() {
     score += 100;
     updateScore();
 }
 
-void drawBoundaries(){
-    for(int row = 0; row < ROWS+2;row++){
-        for(int col = 0; col < COLS+2; col++){
-            if(row == 0 || row == ROWS+1 || col == 0 || col == COLS+1){
-                printf("%c",WALL);         
+void drawBoundaries() {
+    for (int row = 0; row < ROWS + 2; row++) {
+        for (int col = 0; col < COLS + 2; col++) {
+            if (row == 0 || row == ROWS + 1 || col == 0 || col == COLS + 1) {
+                printf("%c", WALL);         
             }
-            else{
+            else {
                 printf(" ");
             }
         }
         printf("\n");
-        
     }
 }
 
-
-void drawSnake(Snake snake){
-    gotoxy(snake.X,snake.Y);
-    printf("@");
+void drawSnake(Snake snake) {
+    for (int i = 0; i < snake.length; i++) {
+        gotoxy(snake.body[i].X, snake.body[i].Y);
+        printf("O");
+    }
 }
 
-void clearSnake(Snake snake){
-    gotoxy(snake.X,snake.Y);
-    printf(" ");
+void clearSnake(Snake snake) {
+    for (int i = 0; i < snake.length; i++) {
+        gotoxy(snake.body[i].X, snake.body[i].Y);
+        printf(" ");
+    }
 }
 
-typedef enum {
-    KeyboardDir_Left,    
-    KeyboardDir_Right,
-    KeyboardDir_Up,
-    KeyboardDir_Down,
-}KeyboardDir;
+void drawApple(Apple apple) {
+    gotoxy(apple.X, apple.Y);
+    printf("A");
+}
 
+void placeApple(Apple *apple) {
+    apple->X = (rand() % COLS) + 2;
+    apple->Y = (rand() % ROWS) + 2;
+}
 
-int kbhit2()
-{
+int kbhit2() {
     return _kbhit();
 }
 
-int getNextKeyboardAction(){
-    if(kbhit2()){
-        //gotoxy(0,ROWS+3);
-        //printf("Ange vad den ska göra:");
+int getNextKeyboardAction() {
+    if (kbhit2()) {
         char ch = getch();
-        //printf("%d",ch);
         return ch;
     }
     return 0;
 }
 
-void moveSnake(Snake *snake,KeyboardDir direction){
+void moveSnake(Snake *snake, KeyboardDir direction) {
+    Position newHead = getNextPosition(*snake, direction);
 
+    // Move the body
+    for (int i = snake->length - 1; i > 0; i--) {
+        snake->body[i] = snake->body[i - 1];
+    }
 
-    if(direction == KeyboardDir_Up){
-        if(snake->Y == 2) snake->Y = ROWS+1;
-        else snake->Y--;
-    }
-    if(direction == KeyboardDir_Down){
-        if(snake->Y == ROWS+1) snake->Y = 2;
-        else snake->Y++;
-    }
-    if(direction == KeyboardDir_Left){
-        if(snake->X == 2) snake->X = COLS+1;
-        else snake->X--;
-    }
-    if(direction == KeyboardDir_Right){
-        if(snake->X == COLS+1) snake->X = 2;
-        else snake->X++;
-    }
-    increaseScore();
-
+    // Place the new head at the front
+    snake->body[0] = newHead;
 }
 
-int main(){
+int main() {
     Snake snake;
-    snake.X = 5; 
-    snake.Y = 5;
+    snake.length = 1;  // Initial length of the snake
+    snake.body[0].X = 5; 
+    snake.body[0].Y = 5;
+    
+
+    Apple apple;
+    srand(time(NULL));  
+    placeApple(&apple);
+
     clrscr();
     hideCursor();
-    drawBoundaries(1);
-    while(1){
-        //clrscr();
-        //drawBoundaries(1);
+    drawBoundaries();
+    
+    while (1) {
         clearSnake(snake);
-        gotoxy(0,0);
+
+        if (snake.body[0].X == apple.X && snake.body[0].Y == apple.Y) {
+            placeApple(&apple);
+            increaseScore();
+            snake.length++;  // Grow the snake by adding a new segment
+        }
+
+        gotoxy(0, 0);
         char ch = getNextKeyboardAction();
-        if(ch == 'w') moveSnake(&snake,KeyboardDir_Up);
-        else if(ch == 's') moveSnake(&snake,KeyboardDir_Down);
-        else if(ch == 'a') moveSnake(&snake,KeyboardDir_Left);
-        else if(ch == 'd') moveSnake(&snake,KeyboardDir_Right);
+        if (ch == 'w') moveSnake(&snake, KeyboardDir_Up);
+        else if (ch == 's') moveSnake(&snake, KeyboardDir_Down);
+        else if (ch == 'a') moveSnake(&snake, KeyboardDir_Left);
+        else if (ch == 'd') moveSnake(&snake, KeyboardDir_Right);
+        
         drawSnake(snake);
+        drawApple(apple);
         msleep(250);
     }
+    
     return 1;
 }
